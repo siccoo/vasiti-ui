304e1801b553984be15efc13b5307962d493f794
siccoo <obichilaka@gmail.com>
2021-02-20 13:23:15 +0100

    images added & home folder component created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
 
+
+
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          
+
         </Switch>
       </Router>
     </div>
