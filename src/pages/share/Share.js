f6c6ec5dc178e3161ac81f09a5c103946e5eb7b6
siccoo <obichilaka@gmail.com>
2021-02-22 12:30:52 +0100

    updates on Share.js

--- src/pages/share/Share.js
+++ src/pages/share/Share.js
@@ -12,6 +12,20 @@ const Share = () => {
                     <div className="row">
                         <div className="col-md-12">
                             <h3>Share your amazing story</h3>
+                            <form>
+                                <div class="form-group">
+                                    <label for="exampleFormControlInput1">Email address</label>
+                                    <input type="email" id="email" placeholder="name@example.com" />
+                                </div>
+                                <div class="row">
+                                    <div class="col">
+                                        <input type="text" class="form-control" placeholder="First name" />
+                                    </div>
+                                    <div class="col">
+                                        <input type="text" class="form-control" placeholder="Last name" />
+                                    </div>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
