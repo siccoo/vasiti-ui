9c07a8fe40cf53a81c2c765cdaedd5ad0703bee3
siccoo <obichilaka@gmail.com>
2021-02-22 14:53:18 +0100

    updates on Share.css

--- src/pages/share/Share.js
+++ src/pages/share/Share.js
@@ -1,10 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Share.css";
 
 const Share = () => {
     useEffect(() => {
         document.title = "Vasiti | Share your amazing story";
     }, []);
+
+    const [fname, setFname] = useState("");
+    const [lname, setLname] = useState("");
+    const [story, setStory] = useState("");
+    const [item, setItem] = useState([])
+
+    const handleSubmit = (e) => {
+
+        e.preventDefault();
+        setItem([...item,{fname:fname, lname:lname, story:story}])
+        setFname('')
+        setLname('')
+        setStory('')
+      };
+
     return (
         <div>
             <section className="portion">
@@ -12,24 +27,29 @@ const Share = () => {
                     <div className="row">
                         <div className="col-md-12">
                             <h3 className="text-center">Share your amazing story</h3>
-                            <form className="text-left">
+                            <form className="text-left" onSumbmit={handleSubmit}>
                                 <div className="form-group">
                                     <label for="exampleFormControlInput1">Upload your picture</label>
                                     <input type="image" id="image" placeholder="Choose image" />
+                                    {/* <div class="image-input">
+	                                    <input type="file" accept="image/*" id="imageInput" />
+	                                    <label for="imageInput" class="image-button"> Choose image</label>
+	                                    
+                                    </div> */}
                                 </div>
                                 <div className="row">
                                     <div className="col">
                                         <label for="first-name">First Name</label>
-                                        <input type="text" placeholder="Oluwatioluwalase" />
+                                        <input type="text" name="fname" value={fname} onChange={(e) => setFname(e.target.value)} placeholder="Oluwatioluwalase" />
                                     </div>
                                     <div className="col">
                                         <label for="last-name">Last Name</label>
-                                        <input type="text" placeholder="Wellington" />
+                                        <input type="text" name="lname" value={lname} onChange={(e) => setLname(e.target.value)} placeholder="Wellington" />
                                     </div>
                                 </div>
                                 <div className="form-group">
                                     <label for="share">Share your story</label>
-                                    <textarea name="message" cols="20" rows="5" className="upLabel"  placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud " onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Message'" />
+                                    <textarea name="story" value={story} onChange={(e) => setStory(e.target.value)} cols="20" rows="5" className="upLabel"  placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud " onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Message'" />
                                 </div>
                                 <div className="form-check form-check-inline ">
                                     <label>What did you interact with Vasiti as?</label>
