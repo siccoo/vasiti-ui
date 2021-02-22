f68fe31b8cb37bea0405e37b077a6b48a368094f
siccoo <obichilaka@gmail.com>
2021-02-22 12:46:54 +0100

    updates on Share.js

--- src/pages/share/Share.js
+++ src/pages/share/Share.js
@@ -13,21 +13,34 @@ const Share = () => {
                         <div className="col-md-12">
                             <h3>Share your amazing story</h3>
                             <form>
-                                <div class="form-group">
+                                <div className="form-group">
                                     <label for="exampleFormControlInput1">Email address</label>
                                     <input type="email" id="email" placeholder="name@example.com" />
                                 </div>
-                                <div class="row">
-                                    <div class="col">
+                                <div className="row">
+                                    <div className="col">
                                         <input type="text" placeholder="First name" />
                                     </div>
-                                    <div class="col">
+                                    <div className="col">
                                         <input type="text" placeholder="Last name" />
                                     </div>
                                 </div>
                                 <div className="form-group">
                                     <label for="help">How May We Help You <span>*</span></label>
                                     <textarea name="message" cols="20" rows="5" className="upLabel"  placeholder="Tell us anything" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Message'" />
+                                </div>
+                                <div className="col-md-7">
+                                    <p>What did you interact with Vasiti as?</p>
+                                </div>
+                                <div className="col-md-5">
+                                    <div className="form-check form-check-inline">
+                                        <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" />
+                                        <label className="form-check-label" for="inlineRadio1">1</label>
+                                    </div>
+                                    <div className="form-check form-check-inline">
+                                        <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" />
+                                        <label className="form-check-label" for="inlineRadio2">2</label>
+                                    </div>
                                 </div>
                             </form>
                         </div>
